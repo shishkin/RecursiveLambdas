e4c5a93d78b4ddd4be8d94ddc7fe435be5219a66
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-02 20:21:30 +0200

    Implemented factorial

--- Program.cs
+++ Program.cs
@@ -9,9 +9,20 @@ namespace RecursiveLambdas
     {
         public delegate T Self<T>(Self<T> self);
 
+        public static readonly Self<Func<Func<Func<int, int>, Func<int, int>>, Func<int, int>>> Y
+            = y => f => x => f(y(y)(f))(x);
+
+        public static readonly Func<Func<Func<int, int>, Func<int, int>>, Func<int, int>> Fix
+            = Y(Y);
+
         static void Main(string[] args)
         {
+            var factorial = Fix(fac => x => x == 0 ? 1 : x * fac(x - 1));
 
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine(factorial(i));
+            }
         }
     }
 }
