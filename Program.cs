3f86b6950c9c2f1b979a52e754c914c3495b3f3d
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-02 21:15:59 +0200

    Generalized Fixed Point function

--- Program.cs
+++ Program.cs
@@ -3,16 +3,33 @@ using System.Linq;
 
 namespace RecursiveLambdas
 {
+    public static class LambdaEx
+    {
+        public static Func<TResult> Recursive<TResult>(Func<Func<TResult>, Func<TResult>> f)
+        {
+            return () => f(Recursive(f))();
+        }
+
+        public static Func<T, TResult> Recursive<T, TResult>(Func<Func<T, TResult>, Func<T, TResult>> f)
+        {
+            return x => f(Recursive(f))(x);
+        }
+
+        public static Func<T1, T2, TResult> Recursive<T1, T2, TResult>(Func<Func<T1, T2, TResult>, Func<T1, T2, TResult>> f)
+        {
+            return (arg1, arg2) => f(Recursive(f))(arg1, arg2);
+        }
+
+        // And so on...
+    }
+
     internal class Program
     {
-        public static Func<T, T> Fix<T>(Func<Func<T, T>, Func<T, T>> f)
-        {
-            return x => f(Fix(f))(x);
-        }
 
         private static void Main()
         {
-            var factorial = Fix<int>(self => x => x == 0 ? 1 : x*self(x - 1));
+            var factorial = LambdaEx.Recursive<int, int>(
+                self => x => x == 0 ? 1 : x*self(x - 1));
 
             for (var i = 0; i < 12; i++)
             {
