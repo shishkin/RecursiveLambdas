c6f0f13a352a57458694f8a52592a6fd9f8e506f
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-02 20:13:48 +0200

    Added self delegate

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace RecursiveLambdas
 {
     class Program
     {
+        public delegate T Self<T>(Self<T> self);
+
         static void Main(string[] args)
         {
+
         }
     }
 }
