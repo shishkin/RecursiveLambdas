459d39ec67905f8c26d68a83f801c058857fa452
Sergey Shishkin <sergei.shishkin@gmail.com>
2010-07-02 20:55:29 +0200

    Minor refactorings

--- Program.cs
+++ Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace RecursiveLambdas
 {
-    class Program
+    internal class Program
     {
         public static Func<T, T> Fix<T>(Func<Func<T, T>, Func<T, T>> f)
         {
             return x => f(Fix(f))(x);
         }
 
-        static void Main(string[] args)
+        private static void Main()
         {
-            var factorial = Fix<int>(fac => x => x == 0 ? 1 : x * fac(x - 1));
+            var factorial = Fix<int>(self => x => x == 0 ? 1 : x*self(x - 1));
 
-            for (int i = 0; i < 12; i++)
+            for (var i = 0; i < 12; i++)
             {
                 Console.WriteLine(factorial(i));
             }
